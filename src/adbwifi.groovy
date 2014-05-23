1a2fc71340d13215f4826511a6e0e03be636b0ec
dhelleberg <helleberg@gmx.de>
2014-05-23 22:14:04 +0200

    fixed some edge cases. wifi turn on works in simple situations

--- src/adbwifi.groovy
+++ src/adbwifi.groovy
@@ -4,19 +4,18 @@
  */
 
 //get adb exec
-def adbExec = getAdbPath();
+adbExec = getAdbPath();
 
 //check connected devices
 def adbDevicesCmd = "$adbExec devices"
 def proc = adbDevicesCmd.execute()
-
 proc.waitFor()
 
 def foundDevice = false
 
-proc.in.text.eachLine {
+proc.in.text.eachLine { //start at line 1 and check for a connected device
         line, number ->
-            if(number == 1 && line.contains("device"))
+            if(number == 1 && line.contains("device"))  //Todo this could fail because it also finds remote devices
                 foundDevice = true
 }
 
@@ -25,26 +24,16 @@ if(!foundDevice) {
     System.exit(-1)
 }
 
-//device is there, check if wifi is running
-def adbshellNetcfgCmd = "$adbExec -d shell netcfg"
-proc = adbshellNetcfgCmd.execute()
 
-proc.waitFor()
-
-def wifiup = true
-def wlanIP = ""
-proc.in.text.eachMatch(/wlan.*/) {
-    if(it.contains("UP")) {
-        //try to get ip address
-        def matcher = (it =~ /([a-zA-Z|0]+)?([0-9|.]+)/)
-        wlanIP = matcher[1][0]
-        if(wlanIP == "0.0.0.0")
-            wifiup = false
-        else
-            println "mobile ip on WLAN: $wlanIP"
-    }
+def wlanIP = getWlanIP(true)
+if(wlanIP == "0.0.0.0") {
+    println("could not determine wlan ip. exit")
+    System.exit(-1)
 }
 
+
+
+println "mobile ip on WLAN: $wlanIP"
 def adbConnect = "$adbExec tcpip 5555"
 adbConnect.execute()
 
@@ -54,37 +43,68 @@ System.console().readLine()
 
 //now connect the device
 def adbConnectWifi = "$adbExec connect $wlanIP"
-adbConnectWifi.execute()
+proc = adbConnectWifi.execute()
+proc.waitFor()
 
+def adbDevices = "$adbExec devices"
+proc = adbDevices.execute()
+proc.waitFor()
+proc.in.text.eachLine { println(it) }
 
+private String getWlanIP(boolean tryTurnItOn) {
+    //device is there, check if wifi is running
+    def adbshellNetcfgCmd = "$adbExec -d shell netcfg"
+    proc = adbshellNetcfgCmd.execute()
+    proc.waitFor()
 
-if(!wifiup) {
-    turnWifiOn()
+    def ip
+
+    proc.in.text.eachMatch(/wlan.*/) {
+        if(it.contains("UP")) {
+            //try to get ip address
+            def matcher = (it =~ /([a-zA-Z|0]+)?([0-9|.]+)/)
+            String wlanIP = matcher[1][0]
+            println "WLAN IP $wlanIP"
+            if(wlanIP == "0.0.0.0" && tryTurnItOn) { //no wifi yet, try to get it up
+                turnWifiOn()
+                //on more try
+                ip = getWlanIP(false)
+            }
+            else
+                ip = wlanIP
+        }
+    }
+
+    return ip
+
 }
-
-
-//println "return code: ${ proc.exitValue()}"
-
 
 private void turnWifiOn() {
     //try to turn wifi on
     def startWifiManager = "adb shell am start -a android.intent.action.MAIN -n com.android.settings/.wifi.WifiSettings"
-    startWifiManager.execute()
-    def switchWifiOn = "adb shell input keyevent 20 & adb shell input keyevent 23"
-    switchWifiOn.execute()
+    p = startWifiManager.execute()
+    p.waitFor()
+    def switchWifiOn = "adb shell input keyevent 20"
+    p = switchWifiOn.execute()
+    p.waitFor()
+
+    switchWifiOn = "adb shell input keyevent 23"
+    p = switchWifiOn.execute()
+    p.waitFor()
+
     //give it a few seconds to get an ip
     sleep(2000)
 }
 
 private String getAdbPath() {
-    def adbExec = "adb"
+    def adbExec = "adb"   //Todo: check if we need adb.exe on windows
     try {
-        def command = "$adbExec"// is actually a string
+        def command = "$adbExec"    //try it plain from the path
         command.execute()
         return adbExec
     }
     catch (IOException e) {
-        //try with to get Android Home
+        //next try with Android Home
         def env = System.getenv("ANDROID_HOME")
         println("adb not in path trying Android home")
         if (env != null && env.length() > 0) {
@@ -96,19 +116,10 @@ private String getAdbPath() {
                 return adbExec
             }
             catch (IOException ex) {
-                println("Could not find adb in path and no ANDROID_HOME is set :(")
-                System.exit(0)
+                println("Could not find $adbExec in path and no ANDROID_HOME is set :(")
+                System.exit(-1)
             }
         }
     }
 }
 
-
-def command = """adbs shell netcfg"""// Create the String
-/*proc.waitFor()                               // Wait for the command to finish
-
-// Obtain status and output
-println "return code: ${ proc.exitValue()}"
-println "stderr: ${proc.err.text}"
-println "stdout: ${proc.in.text}" // *out* from the external program is *in* for groovy
-//proc.in.text.eachLine {"each:"+println(it)}*/
