278988a75605cfb3e5ef6d6ad4b5a0aa5d8767f8
aminezghidi <zghidi.amine@gmail.com>
2016-10-12 21:59:29 +0100

    Add +format command to add or subtract days, hours, minutes, or seconds date +%d -%d +%h -%h +%m -%m +%s -%s

--- src/devtools.groovy
+++ src/devtools.groovy
@@ -1,41 +1,75 @@
 #!/usr/bin/env groovy
 
+@Grab('joda-time:joda-time:2.9.4')
+import org.joda.time.DateTime
+import org.joda.time.format.*
+
 /**
  * Created by dhelleberg on 24/09/14.
  * Improve command line parsing
  */
 
-gfx_command_map = ['on' : 'visual_bars', 'off' : 'false', 'lines' : 'visual_lines']
-layout_command_map = ['on' : 'true', 'off' : 'false']
-overdraw_command_map = ['on' : 'show',  'off' : 'false', 'deut' : 'show_deuteranomaly']
-overdraw_command_map_preKitKat = ['on' : 'true',  'off' : 'false']
-show_updates_map = ['on' : '0',  'off' : '1']
+gfx_command_map = ['on': 'visual_bars', 'off': 'false', 'lines': 'visual_lines']
+layout_command_map = ['on': 'true', 'off': 'false']
+overdraw_command_map = ['on': 'show', 'off': 'false', 'deut': 'show_deuteranomaly']
+overdraw_command_map_preKitKat = ['on': 'true', 'off': 'false']
+show_updates_map = ['on': '0', 'off': '1']
+date_single_option_possibilites = ['reset']
+date_format_supported = ['d', 'h', 'm', 's']
+date_opration_supported = ['+', '-']
 
-command_map = ['gfx' : gfx_command_map,
-               'layout' : layout_command_map,
-               'overdraw' : overdraw_command_map,
+ERROR_DATE_CMD_OPTION = 0
+
+command_map = ['gfx'     : gfx_command_map,
+               'layout'  : layout_command_map,
+               'overdraw': overdraw_command_map,
                'updates' : show_updates_map]
 
 verbose = false
 
-def cli = new CliBuilder(usage:'devtools.groovy command option')
+def cli = new CliBuilder(usage: 'devtools.groovy command option')
 cli.with {
     v longOpt: 'verbose', 'prints additional output'
 }
 def opts = cli.parse(args)
-if(!opts)
+if (!opts)
     printHelp("not provided correct option")
-if(opts.arguments().size() != 2)
-   printHelp("you need to provide two arguments: command and option")
-if(opts.v)
+
+if (opts.v)
     verbose = true
 
 //get args
 String command = opts.arguments().get(0)
-String option = opts.arguments().get(1)
+String option
+options = new String[opts.arguments().size() - 1]
+
+switch (command) {
+    case "gfx":
+    case "layout":
+    case "overdraw":
+    case "updates":
+        if (opts.arguments().size() != 2)
+            printHelp("you need to provide two arguments: command and option")
+        option = opts.arguments().get(1)
+        break
+
+    case "date":
+        for (int i = 0; i < options.length; i++) {
+            options[i] = opts.arguments().get(i + 1)
+        }
+
+        if (options.size() == 0)
+            printHelpForSpecificCommand(command)
+
+        if (options.size() == 1) {
+            if (!isAValidDateSingleOption(options[0]) && !isAValidDateOption(options[0])) {
+                printHelpForSpecificCommand(command)
+            }
+        }
+}
 
 //get adb exec
-adbExec = getAdbPath();
+adbExec = getAdbPath()
 
 //check connected devices
 def adbDevicesCmd = "$adbExec devices"
@@ -45,50 +79,51 @@ proc.waitFor()
 def foundDevice = false
 deviceIds = []
 
-proc.in.text.eachLine { //start at line 1 and check for a connected device
-line, number ->
-    if(number > 0 && line.contains("device")) {
-        foundDevice = true
-        //grep out device ids
-        def values = line.split('\\t')
-        if(verbose)
-            println("found id: "+values[0])
-        deviceIds.add(values[0])
-    }
+proc.in.text.eachLine {
+        //start at line 1 and check for a connected device
+    line, number ->
+        if (number > 0 && line.contains("device")) {
+            foundDevice = true
+            //grep out device ids
+            def values = line.split('\\t')
+            if (verbose)
+                println("found id: " + values[0])
+            deviceIds.add(values[0])
+        }
 }
 
-if(!foundDevice) {
+if (!foundDevice) {
     println("No usb devices")
     System.exit(-1)
 }
 
 def adbCmd = ""
-switch ( command ) {
-    case "gfx" :
-        adbCmd = "shell setprop debug.hwui.profile "+gfx_command_map[option]
+switch (command) {
+    case "gfx":
+        adbCmd = "shell setprop debug.hwui.profile " + gfx_command_map[option]
         executeADBCommand(adbCmd)
         break
 
-    case "layout" :
-        adbCmd = "shell setprop debug.layout "+layout_command_map[option]
+    case "layout":
+        adbCmd = "shell setprop debug.layout " + layout_command_map[option]
         executeADBCommand(adbCmd)
         break
 
-    case "overdraw" :
+    case "overdraw":
         //tricky, properties have changed over time
-        adbCmd = "shell setprop debug.hwui.overdraw "+overdraw_command_map[option]
+        adbCmd = "shell setprop debug.hwui.overdraw " + overdraw_command_map[option]
         executeADBCommand(adbCmd)
-        adbCmd = "shell setprop debug.hwui.show_overdraw "+overdraw_command_map_preKitKat[option]
+        adbCmd = "shell setprop debug.hwui.show_overdraw " + overdraw_command_map_preKitKat[option]
         executeADBCommand(adbCmd)
         break
 
     case "updates":
-        adbCmd = "shell service call SurfaceFlinger 1002 android.ui.ISurfaceComposer"+show_updates_map[option]
+        adbCmd = "shell service call SurfaceFlinger 1002 android.ui.ISurfaceComposer" + show_updates_map[option]
         executeADBCommand(adbCmd)
         break
 
     case "date":
-        adbCmd = buildDateCommand(option);
+        adbCmd = buildDateCommand()
         executeADBCommand(adbCmd)
         break
 
@@ -98,122 +133,14 @@ switch ( command ) {
 }
 
 kickSystemService()
-
 System.exit(0)
 
-void kickSystemService() {
-    def proc
-    int SYSPROPS_TRANSACTION = 1599295570 // ('_'<<24)|('S'<<16)|('P'<<8)|'R'
-
-    def pingService = "shell service call activity $SYSPROPS_TRANSACTION"
-    executeADBCommand(pingService)
-}
-
-void executeADBCommand(String adbCmd) {
-    if(deviceIds.size == 0) {
-        println("no devices connected")
-        System.exit(-1)
-    }
-    deviceIds.each { deviceId ->
-        def proc
-        def adbConnect = "$adbExec -s $deviceId $adbCmd"
-        if(verbose)
-            println("Executing $adbConnect")
-        proc = adbConnect.execute()
-        proc.waitFor()
-    }
-}
-
-String getAdbPath() {
-    def adbExec = "adb"
-    if(isWindows())
-        adbExec = adbExec+".exe"
-    try {
-        def command = "$adbExec"    //try it plain from the path
-        command.execute()
-        if(verbose)
-            println("using adb in "+adbExec)
-        return adbExec
-    }
-    catch (IOException e) {
-        //next try with Android Home
-        def env = System.getenv("ANDROID_HOME")
-        if(verbose)
-            println("adb not in path trying Android home")
-        if (env != null && env.length() > 0) {
-            //try it here
-            try {
-                adbExec = env + File.separator + "platform-tools" + File.separator + "adb"
-                if(isWindows())
-                    adbExec = adbExec+".exe"
-
-                def command = "$adbExec"// is actually a string
-                command.execute()
-                if(verbose)
-                    println("using adb in "+adbExec)
-
-                return adbExec
-            }
-            catch (IOException ex) {
-                println("Could not find $adbExec in path and no ANDROID_HOME is set :(")
-                System.exit(-1)
-            }
-        }
-        println("Could not find $adbExec in path and no ANDROID_HOME is set :(")
-        System.exit(-1)
-    }
-}
-
-boolean isWindows() {
-    return (System.properties['os.name'].toLowerCase().contains('windows'))
-}
-
-void printHelp(String additionalmessage) {
-    println("usage: devtools.groovy [-v] command option")
-    print("command: ")
-    command_map.each { command, options ->
-        print("\n  $command -> ")
-        options.each {
-            option, internal_cmd -> print("$option ")
-        }
-    }
-    println()
-    println("Error $additionalmessage")
-    println()
-
-    System.exit(-1)
-}
-
-private boolean isNOrLater() {
-    GString apiLevelCmd = "$adbExec shell getprop ro.build.version.sdk";
-    proc = apiLevelCmd.execute()
-    proc.waitFor()
-
-    Integer apiLevel = 0
-    proc.in.text.eachLine { apiLevel = it.toInteger() }
-    if (apiLevel == 0) {
-        println("Could not retrieve API Level")
-        System.exit(-1)
-    } else {
-        if (apiLevel >= 24) {
-            return true
-        } else {
-            return false
-        }
-    }
-}
+/* CMD METHODS */
 
 String fixFormat(String val) {
     if (val.length() == 1)
-        return "0" + val;
-    return val;
-}
-
-String buildDateCommand(String option) {
-    switch (option) {
-        case "reset":
-            return buildResetCommand();
-    }
+        return "0" + val
+    return val
 }
 
 String buildResetCommand() {
@@ -247,4 +174,283 @@ String buildResetCommand() {
                 secondsOfMinutes
     }
     return adbCmd
+}
+
+String buildDateCommand() {
+    if (options.size() == 1 && isAValidDateSingleOption(options[0])) {
+        buildResetCommand()
+
+    } else {
+        DateTime deviceDateTime = getDeviceDateTime()
+        String resultMessage = "Date changed from " + deviceDateTime + " to "
+
+        options.each { option ->
+            if (option.length() > 4 || option.length() < 3) {
+                printHelp(ERROR_DATE_CMD_OPTION, option)
+            }
+
+            def operation = option.take(1)
+            def rangeType = option.reverse().take(1).reverse()
+
+            if (!(operation in date_opration_supported)) {
+                printHelp(ERROR_DATE_CMD_OPTION, option)
+            }
+
+            if (!(rangeType in date_format_supported)) {
+                printHelp(ERROR_DATE_CMD_OPTION, option)
+            }
+
+            def range = option.substring(1, option.length() - 1)
+            if (!range.isNumber()) {
+                printHelp(ERROR_DATE_CMD_OPTION, option)
+            }
+
+            deviceDateTime = applyRangeToDate(deviceDateTime, operation, Integer.valueOf(range), rangeType)
+        }
+
+        resultMessage += deviceDateTime
+        println(resultMessage)
+
+        String formattedDate = formatDateForAdbCommand(deviceDateTime)
+
+        if (isNOrLater()) {
+            adbCmd = "shell date " + formattedDate
+
+        } else {
+            adbCmd = "shell date -s " + formattedDate
+        }
+        println(adbCmd)
+
+        return adbCmd
+
+    }
+}
+
+private boolean isAValidDateOption(String option) {
+    def operation = option.take(1)
+    def rangeType = option.reverse().take(1).reverse()
+
+    if (!(operation in date_opration_supported)) {
+        return false
+    }
+
+    if (!(rangeType in date_format_supported)) {
+        return false
+    }
+
+    def range = option.substring(1, option.length() - 1)
+    if (!range.isNumber()) {
+        return false
+    }
+
+    return true
+}
+
+private boolean isAValidDateSingleOption(String option) {
+    if (option in date_single_option_possibilites)
+        return true
+
+    return false
+}
+
+private DateTime applyRangeToDate(DateTime dateTime, def operation, int range, def rangeType) {
+    if (operation.equals("+")) {
+        return addRange(dateTime, rangeType, range)
+    } else {
+        return minusRange(dateTime, rangeType, range)
+    }
+}
+
+private DateTime addRange(DateTime fromDate, def rangeType, int range) {
+    switch (rangeType) {
+        case "d":
+            return fromDate.plusDays(range)
+
+        case "h":
+            return fromDate.plusHours(range)
+
+        case "m":
+            return fromDate.plusMinutes(range)
+
+        case "s":
+            return fromDate.plusSeconds(range)
+    }
+}
+
+private DateTime minusRange(DateTime fromDate, def rangeType, int range) {
+    switch (rangeType) {
+        case "d":
+            return fromDate.minusDays(range)
+            break
+
+        case "h":
+            return fromDate.minusHours(range)
+            break
+
+        case "m":
+            return fromDate.minusMinutes(range)
+            break
+
+        case "s":
+            return fromDate.minusSeconds(range)
+    }
+}
+
+private String getDeviceDate() {
+    if (isNOrLater()) {
+        adbCmd = "shell date +%Y%m%d.%H%M%S"
+    } else {
+        adbCmd = "shell date +%Y%m%d.%H%M%S"
+    }
+    return executeADBCommand(adbCmd)
+}
+
+private DateTime getDeviceDateTime() {
+    deviceDate = getDeviceDate()
+    if (verbose)
+        println("Device current Date: " + deviceDate)
+
+    int year = Integer.valueOf(deviceDate.take(4))
+    int month = Integer.valueOf(deviceDate[4..5])
+    int day = Integer.valueOf(deviceDate[6..7])
+    int hours = Integer.valueOf(deviceDate[9..10])
+    int minutes = Integer.valueOf(deviceDate[11..12])
+    int seconds = Integer.valueOf(deviceDate[13..14])
+
+    return new DateTime(year, month, day, hours, minutes, seconds)
+}
+
+private String formatDateForAdbCommand(DateTime dateTime) {
+    def dateFormat
+    if (isNOrLater()) {
+        dateFormat = "MMddHHmmYYYY.ss"
+    } else {
+        dateFormat = "YYYYMMd.HHMMss"
+    }
+
+    DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern(dateFormat)
+    return dateTime.toString(dateTimeFormatter)
+}
+
+/* print help */
+
+void printHelp(String additionalmessage) {
+    println("usage: devtools.groovy [-v] command option")
+    print("command: ")
+    command_map.each { command, options ->
+        print("\n  $command -> ")
+        options.each {
+            option, internal_cmd -> print("$option ")
+        }
+    }
+    println()
+    println("Error $additionalmessage")
+    println()
+
+    System.exit(-1)
+}
+
+void printHelp(def err, String additionalMessage) {
+    switch (err) {
+        case ERROR_DATE_CMD_OPTION:
+            println("Error in this command option: " + additionalMessage)
+            printHelpForSpecificCommand("date " + additionalMessage)
+            break
+    }
+    System.exit(-1);
+}
+
+void printHelpForSpecificCommand(String command) {
+    println("usage: devtools.groovy [-v] command option")
+    println("Not valid command: " + command)
+    System.exit(-1)
+}
+
+/* ADB UTILS */
+
+String executeADBCommand(String adbCmd) {
+    if (deviceIds.size == 0) {
+        println("no devices connected")
+        System.exit(-1)
+    }
+    def proc
+    deviceIds.each { deviceId ->
+        def adbConnect = "$adbExec -s $deviceId $adbCmd"
+        if (verbose)
+            println("Executing $adbConnect")
+        proc = adbConnect.execute()
+        proc.waitFor()
+    }
+    return proc.text
+}
+
+String getAdbPath() {
+    def adbExec = "adb"
+    if (isWindows())
+        adbExec = adbExec + ".exe"
+    try {
+        def command = "$adbExec"    //try it plain from the path
+        command.execute()
+        if (verbose)
+            println("using adb in " + adbExec)
+        return adbExec
+    }
+    catch (IOException e) {
+        //next try with Android Home
+        def env = System.getenv("ANDROID_HOME")
+        if (verbose)
+            println("adb not in path trying Android home")
+        if (env != null && env.length() > 0) {
+            //try it here
+            try {
+                adbExec = env + File.separator + "platform-tools" + File.separator + "adb"
+                if (isWindows())
+                    adbExec = adbExec + ".exe"
+
+                def command = "$adbExec"// is actually a string
+                command.execute()
+                if (verbose)
+                    println("using adb in " + adbExec)
+
+                return adbExec
+            }
+            catch (IOException ex) {
+                println("Could not find $adbExec in path and no ANDROID_HOME is set :(")
+                System.exit(-1)
+            }
+        }
+        println("Could not find $adbExec in path and no ANDROID_HOME is set :(")
+        System.exit(-1)
+    }
+}
+
+void kickSystemService() {
+    def proc
+    int SYSPROPS_TRANSACTION = 1599295570 // ('_'<<24)|('S'<<16)|('P'<<8)|'R'
+
+    def pingService = "shell service call activity $SYSPROPS_TRANSACTION"
+    executeADBCommand(pingService)
+}
+
+boolean isWindows() {
+    return (System.properties['os.name'].toLowerCase().contains('windows'))
+}
+
+private boolean isNOrLater() {
+    GString apiLevelCmd = "$adbExec shell getprop ro.build.version.sdk";
+    proc = apiLevelCmd.execute()
+    proc.waitFor()
+
+    Integer apiLevel = 0
+    proc.in.text.eachLine { apiLevel = it.toInteger() }
+    if (apiLevel == 0) {
+        println("Could not retrieve API Level")
+        System.exit(-1)
+    } else {
+        if (apiLevel >= 24) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
