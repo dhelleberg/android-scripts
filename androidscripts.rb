1cede0de387a7fe18528cfcf1d010a299c447269
aminezghidi <zghidi.amine@gmail.com>
2016-10-13 09:54:09 +0100

    Update androidscripts.rb

--- androidscripts.rb
+++ androidscripts.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Androidscripts < Formula
 
-  homepage 'https://github.com/dhelleberg/android-scripts'
-  url 'https://github.com/dhelleberg/android-scripts/archive/1.0.4.tar.gz'
+  homepage 'https://github.com/thefabulous/android-scripts'
+  url 'https://github.com/thefabulous/android-scripts/archive/master.tar.gz'
   sha256 'fd6b541c647c3fce103d7844e62e7161a769cd4900fe449c5ce74d7a5354dcb1'
-  head 'https://github.com/dhelleberg/android-scripts.git'
+  head 'https://github.com/thefabulous/android-scripts.git'
 
   depends_on "groovy"
 
